dc5c09185439a952f1181091cc5261af82ae9f57
Gal Tsubery <gal.tsubery@gmail.com>
2015-08-02 04:09:17 +0000

    Add test for login

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,8 +1,14 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe "Home page" do
-  scenario "anonymous user visit welcome page" do
+  scenario "anonymous user logs in to the app" do
     visit root_path
-    expect(page).to have_content('Welcome')
+    expect(page).to have_content("Welcome")
+    expect(page).to have_content("Please sign in")
+
+    set_mock_auth
+    click_link "Sign in"
+    expect(page.current_path).to eq('/providers')
+    expect(@page).not_to have_content("Please sign in")
   end
 end
