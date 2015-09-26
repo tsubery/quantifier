4f2fe1871be64206fde728d30144317d82e6468c
Gal Tsubery <gal.tsubery@gmail.com>
2015-09-26 17:51:56 +0000

    Fix data migration to goals table

--- db/migrate/20150908234553_add_metric_key_to_goal.rb
+++ db/migrate/20150908234553_add_metric_key_to_goal.rb
@@ -3,7 +3,7 @@ class AddMetricKeyToGoal < ActiveRecord::Migration
     add_column :goals, :metric_key, :string
     Goal.find_each do |goal|
       p goal.credential_id
-      goal.update metric_key: goal.credential.provider.metrics.keys.first
+      goal.update metric_key: goal.credential.provider.metrics.first.key
     end
     change_column :goals, :metric_key, :string,  null: false
     add_index :goals, :metric_key
