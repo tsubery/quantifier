156eb56b8b3b588a0733c28f823e34d989d161fd
Gal Tsubery <gal.tsubery@gmail.com>
2015-09-29 01:22:19 +0000

    Fix google fit strength training length calculation

--- lib/metrics/googlefit/strength_training_minutes.rb
+++ lib/metrics/googlefit/strength_training_minutes.rb
@@ -6,9 +6,10 @@ ProviderRepo.find!(:googlefit).register_metric :strength_training do |metric|
     points = adapter.fetch_strength
 
     points.each_with_object(Hash.new { 0 }) do |point, scores|
-      ts_epoch = point.start_time_nanos.to_i / 1_000_000_000
-      hour = Time.zone.at(ts_epoch).beginning_of_hour
-      scores[hour] += point.value.first.int_val / 1.minute
+      ts_start = point.start_time_nanos.to_i / 1e9
+      ts_end = point.end_time_nanos.to_i / 1e9
+      hour = Time.zone.at(ts_start).beginning_of_hour
+      scores[hour] += ((ts_end - ts_start)/ 60.0).ceil
     end.map do |ts, value|
       Datapoint.new unique: true, timestamp: ts, value: value
     end
