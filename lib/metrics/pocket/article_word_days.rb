cc7a2e6998e41292692e1065772cce3b4973a6d3
Gal Tsubery <gal.tsubery@gmail.com>
2015-09-29 15:41:50 +0000

    Fix pocket's word ages metric

--- lib/metrics/pocket/article_word_days.rb
+++ lib/metrics/pocket/article_word_days.rb
@@ -5,7 +5,7 @@ ProviderRepo.find!(:pocket).register_metric :article_word_days do |metric|
   metric.block = proc do |adapter|
     now_as_epoch = Time.current.utc.to_i
     value = adapter.articles.map do |article|
-      age = now_as_epoch - article["time_added"].to_i / 1.day.to_i
+      age = (now_as_epoch - article["time_added"].to_i) / 1.day.to_i
       age * article["word_count"].to_i
     end.sum
 
