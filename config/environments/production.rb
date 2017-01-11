07f46f4f5cc36e7814d21bc4b0630974b0af5576
Gal Tsubery <gal.tsubery@gmail.com>
2017-01-11 11:36:41 -0600

    Reduce google-api-client log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #prevent google-api-client from spamming the log file
+  Google::Apis.logger.level = Logger::WARN
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
