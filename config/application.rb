0e0640e45af7e3c72a068e623bf61fe1c74e8e68
Gal Tsubery <gal.tsubery@gmail.com>
2021-02-24 16:35:22 -0500

    Update rails config

--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Quantifier
   class Application < Rails::Application
-    config.generators do |g|
-      g.test_framework :rspec,
-                       fixtures: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: false,
-                       request_specs: true
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.active_record.schema_format = :sql
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
